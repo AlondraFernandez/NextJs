64dc5d7a11751c52d7851df180f5049bf8694ab3
ALONDRA FERNANDEZ <alondra2017gaitan@gmail.com>
2025-01-20 21:40:25 -0300

    filtro

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -2,24 +2,26 @@ import PageTitle from "@/components/PageTitle";
 import Link from "next/link";
 
 export default async function ProductDetailsPage({ params }) {
+    const { id } = params;
+    const response = await fetch(`https://dummyjson.com/products/${id}`);
+    const productDetail = await response.json();
 
-    const { id } = await params
-    const data = await fetch(`https://dummyjson.com/products/${id}`)
-    const productDetail = await data.json()
-
-    if(productDetail.message) {
+    if (response.status !== 200) {
         return (
             <>
                 <PageTitle>Producto no encontrado</PageTitle>
                 <p>El producto con id {id} no existe, intentalo de nuevo</p>
                 <Link href="/product">Volver a productos</Link>
             </>
-        )
+        );
     }
 
     return (
         <>
             <PageTitle>Detalle del producto {id}</PageTitle>
+            <p>{productDetail.title}</p>
+            <p>{productDetail.description}</p>
+            <Link href="/product">Volver a productos</Link>
         </>
     );
 }
